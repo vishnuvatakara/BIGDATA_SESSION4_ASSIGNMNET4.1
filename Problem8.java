d1bc4333311ace0318a67dbd83d0fa1268f13a88
vishnuvatakara <31164986+vishnuvatakara@users.noreply.github.com>
2017-10-29 01:54:00 +0530

    problem8
    8) Write a program in java using Arrays, that sorts the element in descending order.

---
+++ Problem8.java
@@ -0,0 +1,23 @@
+package probles;
+import java.util.Arrays;
+import java.util.Collections;
+ 
+
+public class Problem8 {
+	// A sample Java program to sort a subarray
+	// in descending order using Arrays.sort().
+
+	    public static void main(String[] args)
+	    {
+	        // Note that we have Integer here instead of
+	        // int[] as Collections.reverseOrder doesn't
+	        // work for primitive types.
+	        Integer[] arr = {13, 7, 6, 45, 21, 9, 2, 100};
+	 
+	        // Sorts arr[] in descending order
+	        Arrays.sort(arr, Collections.reverseOrder());
+	 
+	        System.out.printf("Modified arr[] : %s",
+	                          Arrays.toString(arr));
+	    }
+	}
